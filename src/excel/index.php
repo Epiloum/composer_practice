e88bf904f7174a8233c06012f7abde7a84d3f550
Epiloum J. Kim <epiloum@gmail.com>
2021-01-31 00:42:36 +0900

    Adding Temporary Directory

--- src/excel/index.php
+++ src/excel/index.php
@@ -1,32 +1,29 @@
 <?php
+
 require_once '../../vendor/autoload.php';
 
 $spsh = \PhpOffice\PhpSpreadsheet\IOFactory::load('./plain_table.xlsx');
 $data = $spsh->getActiveSheet()->rangeToArray('A1:C50', null, true, true, true);
 
-foreach($data as $k => $v)
-{
-	if(!$v['A'])
-	{
-		echo "END on Row#$k\n";
-		break;
-	}
-	
-	echo implode("\t", $v);
-	echo "\n";
+foreach ($data as $k => $v) {
+    if (!$v['A']) {
+        echo "END on Row#$k\n";
+        break;
+    }
+
+    echo implode("\t", $v);
+    echo "\n";
 }
 
 $spsh = \PhpOffice\PhpSpreadsheet\IOFactory::load('./designed_table.xlsx');
 $data = $spsh->getActiveSheet()->rangeToArray('A1:C50', null, true, true, true);
 
-foreach($data as $k => $v)
-{
-        if(!$v['A'])
-	{
-                echo "END on Row#$k\n";
-                break;
-        }
+foreach ($data as $k => $v) {
+    if (!$v['A']) {
+        echo "END on Row#$k\n";
+        break;
+    }
 
-        echo implode("\t", $v);
-        echo "\n";
+    echo implode("\t", $v);
+    echo "\n";
 }
